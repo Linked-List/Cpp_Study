f21b846ef3073245cee6c4eb568ad92c2f74d131
Linked-List <jleey.97@gmail.com>
2019-11-13 13:26:06 +0900

    Oper Overload

---
+++ 2019_2sem_Cpp/OperOverload_Ex/Main.cpp
@@ -0,0 +1,117 @@
+#include "operOverload.h"
+#include <iostream>
+
+// 귀찮아서 정의파일 안나눔 알아서 보셈.
+
+void func(const MyClass& op){
+    std::cout << "MyClass type 함수 호출" << std::endl;
+}
+void func2(int op){
+    std::cout << "int type 함수 호출" << std::endl;
+}
+
+int main(){
+    std::cout << "1-1. MyClass a; 변수선언" << std::endl;
+    MyClass a; // 기본생성자가 불리는 모습
+    std::cout << "1-2. MyClass* ptr = new MyClass;" << std::endl;
+    MyClass* ptr = new MyClass; // 같은 결과를 보임
+    delete ptr;
+
+    std::cout << "2-1. 3개 짜리 클래스 배열 선언" << std::endl;
+    MyClass b[3]; // 만드는 순간 바로 생성자가 불리는 모습.
+    std::cout << "2-2. new를 이용해 배열 동적할당" << std::endl;
+    ptr = new MyClass[3]; // 완전히 같은 결과를 보임
+    delete[] ptr;
+
+    std::cout << "3-1. 복사생성자 호출방식1 - MyClass c(a)" << std::endl;
+    a.setMyNum(10); // 테스트하기위해 멤버변수값 변경
+    MyClass c(a);
+    std::cout << "3-2. 복사생성자 호출방식2 - MyClass d = a (초기화방식)" << std::endl;
+    MyClass d = a;
+    std::cout << "3-3. =연산자 호출 - MyClass e; e = a; (3-2와 비슷해보이지만 다르다.)" << std::endl;
+    MyClass e;
+    e = a;
+
+    std::cout << "4. 변환생성자 호출 - 해당 타입에 해당하는 생성자가 존재하면 알아서 바꿔줌" << std::endl;
+    std::cout << "4-1. 초기화 단계의 호출 - 해당 타입의 생성자를 그냥 부름" << std::endl;
+    MyClass f(1);
+    MyClass g = 1;
+    MyClass h = static_cast<MyClass>(1);
+    std::cout << "4-2. 그외 단계의 호출 - 해당 생성자로 임시 객체를 만든 다음에 대입시킨다." << std::endl;
+    static_cast<MyClass>(1); // 강제 형변환의 가능
+    func(1); // 자동 형변환도 가능. (막으려면 생성자앞에 explicit 선언)
+    
+    std::cout << "4-3. 클래스간에도 가능하나? - 가능하다." << std::endl;
+    MyClass2 aa;
+    aa.myNum=10;
+    MyClass i = aa; // 초기화 단계의 호출
+    static_cast<MyClass>(aa); // 강제형변환
+    func(aa); // 자동형변환
+
+    std::cout << "4-4. 변환 연산자" << std::endl;
+    (int)i; // C스타일 강제형변환
+    static_cast<int>(i); // C++스타일 형변환
+    func2(i); // 자동형변환
+    
+    std::cout << "4-5. 이또한 클래스간 가능?" << std::endl;
+    static_cast<MyClass2>(i); // 가능하다.
+
+    std::cout << "5. 사칙/논리 연산 오버로딩" << std::endl;
+    std::cout << "5-1. + - * / % 관련" << std::endl;
+    h + i;
+    // h + 10;
+    // 이는 우리가 만든 operator int()의 정의로 인해서 int + int로 봐야할지.
+    // myClass + myClass로 봐야할지 몰라서 컴파일에러가 난다.
+    h + static_cast<MyClass>(10); // 모호함은 없애면 될일!
+    // 다른방법은 explicit operator int()로 모호함을 또 해결하면된다.
+    std::cout << "5-2. < > == != <= >= 관련" << std::endl;
+    h < i;
+    std::cout << "5-3. += -= *= /= %= 등 관련" << std::endl;
+    std::cout << (h += i).getMyNum() << std::endl; // return타입을 줬기때문에 이런 행위도 가능
+    std::cout << "5-4. 증감연산자" << std::endl;
+    std::cout << "Original : " << h.getMyNum() << std::endl;
+    std::cout << (++h).getMyNum() << std::endl;
+    std::cout << (h++).getMyNum() << std::endl;
+    
+    std::cout << "5-5. 배열연산자" << std::endl;
+    h[0] = 10;
+    const MyClass& p2 = h;
+    p2[0];
+    std::cout << "5-6. 함수연산자" << std::endl;
+    h(10,20,30); // 매개변수는 형식을 내맘대로 바꿀수있다.
+    // C++에서 이걸 이용해 Java의 Comparator를 구현한다.
+    // C++ Functor 검색
+
+    std::cout << "6-1. IO연산자" << std::endl;
+    std::cin >> h;
+    std::cout << h << std::endl; // 잘 들어간 모습
+
+    /*
+    추가설명
+    어떠한 연산으로 나온 결과 객체는 임시값이다.
+    예) int num = 1+2; 여기서 3이 임시값이다.
+    이 임시은 보통 오른쪽에 항상 오기때문에 R-Value라고 부른다.
+    사용이 종료되면 없어지는 값이다.
+    즉 사용을 계속하면 안없앨수가 있다. 이를 잡아주는 방식이 두가지이다.
+    const int& / 좌측값 int type과, 우측값 int type 모두를 참조할수있다.
+    int&& / 우측값만 지정해서 참조한다. (좌측값이 오면 오류남)
+    다만 전자의 경우 const이기때문에 값을 변경하지 못한다.
+
+    이걸 왜 알아야하냐.
+    return type이 MyClass이면 반환된 그 시점에는 저장이 안되있기때문에 R-Value이다.
+    a+(b+c)를 하면 b+c의 값이 임시값으로 a의 operator+의 인자로 전달된다.
+    따라서 operator+(int& a)로 정의하면 에러가 납니다.
+    -> operator+(const int& a)로 정의하거나 r-value를 위한 operator+(int&& a)를 추가로 정의해야한다.
+    */
+    int myNum = 1;
+    const int& p = 10+20;
+    const int& p3 = myNum;
+    int&& p4 = 10; // 이것또한 우측값이다.
+    //int&& p5 = myNum; 에러
+    p4++; // 이 임시값을 변경도 가능하다. **10이라는 리터럴을 바꾸는게 아니다.**
+    std::cout << "리터럴 10 : " << 10 << std::endl;
+    std::cout << "임시값 p4 증가 : " << p4 << std::endl;
+
+
+    // TODO: new, delete 추가예정
+}
---
+++ 2019_2sem_Cpp/OperOverload_Ex/operOverload.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <iostream>
+
+class MyClass2{
+public:
+    int myNum;
+};
+
+class MyClass{
+private:
+    int myNum;
+    int arr[10];
+
+public:
+    MyClass(){
+        std::cout << "기본생성자 호출" << std::endl;
+        myNum = 0;
+        for(int i=0;i<10;i++)
+            arr[i] = 0;
+    } // MyClass a; 혹은 new MyClass()로 호출할때 불림
+
+    // 복사생성자
+    MyClass(const MyClass& op){
+        std::cout << "복사생성자 호출" << std::endl;
+        myNum = op.myNum;
+        for(int i=0;i<10;i++)
+            arr[i] = op.arr[i];
+    }
+    // = 연산자
+    void operator=(const MyClass& op){
+        std::cout << "=연산자 호출" << std::endl;
+        myNum = op.myNum;
+        for(int i=0;i<10;i++)
+            arr[i] = op.arr[i];
+    }
+
+    // 변환생성자
+    // Explicit
+    MyClass(const int& op){
+        std::cout << "int 타입생성자 혹은 변환생성자 호출" << std::endl;
+        myNum = op;
+        for(int i=0;i<10;i++)
+            arr[i] = 0;
+    }
+    MyClass(const MyClass2& op){
+        std::cout << "MyClass2 타입생성자 혹은 변환생성자 호출" << std::endl;
+        myNum = op.myNum;
+        for(int i=0;i<10;i++)
+            arr[i] = 0;
+    }
+    // 변환연산자 - 왜 리턴타입이 없을까? 리턴타입이 함수이름이자너
+    // 여기또한 explicit 선언가능
+    operator int(){
+        std::cout << "(int) 변환연산자 호출" << std::endl;
+        return myNum;
+    }
+    operator MyClass2(){
+        std::cout << "(MyClass2) 변환연산자 호출" << std::endl;
+        MyClass2 a;
+        a.myNum = this->myNum;
+        return a;
+    }
+
+    // + - * / %
+    // 왼쪽 놈이 함수 operator+를 호출하고, 매개변수로 오른쪽놈이 들어감.
+    MyClass operator+(const MyClass& op){
+        MyClass t;
+        t.myNum = myNum + op.myNum;
+        std::cout << "operator+ 호출" << std::endl;
+        return t;
+    }
+    // > < == != <= >=
+    // && || !는 안하는거 추천
+    // 사용방식은 위와 똑같은데, 보통 반환형이 bool이라서 그냥 나눠봤다.
+    bool operator<(const MyClass& op){
+        std::cout << "operator< 호출" << std::endl;
+        return myNum<op.myNum;
+    }
+
+    // += -= *= /= %=
+    // 일단 사칙연산과의 차이점으로는, 함수 호출하는 애의 값 자체를 바꾼다.
+    // =관련 연산자는 void로 해도되고 MyClass&로 해도된다.
+    // 전후의 차이는 a = (b = c) 같은것이 가능하냐 이다(void는 안됨).
+    MyClass& operator+=(const MyClass& op){
+        std::cout << "operator+= 호출" << std::endl;
+        myNum += op.myNum;
+        for(int i=0;i<10;i++)
+            arr[i] += op.arr[i];
+        return *(this);
+    }
+
+    // ++ --
+    // (int)부분은 의미가 있는 타입이 아니라, 그냥 문법이다. 헷갈리지 말기
+    MyClass& operator++(){
+        std::cout << "전위연산++ 호출" << std::endl;
+        myNum++;
+        return *(this);
+    }
+    const MyClass operator++(int){
+        std::cout << "후위연산++ 호출" << std::endl;
+        MyClass a = *this; // 나를 기준으로 임시객체 만들어놓고.
+        myNum++;
+        // 원래껀 올리고 임시객체를 반환한다.
+        return a;
+    } // 추가 연산못하게 const선언
+
+    // []
+    int& operator[](const int index){
+        std::cout << "일반변수로 []호출" << std::endl;
+        return arr[index];
+    }
+    const int operator[](const int index) const{ // 뒤에 있는 const의 차이
+        std::cout << "Const 객체로 []호출" << std::endl;
+        return arr[index];
+    }
+    // ( )
+    void operator()(int n1, int n2, int n3){
+        std::cout << "operator() 호출 " << n1 << " " << n2 << " " << n3 << std::endl;
+    }
+
+    // << , >>
+    // friend함수는 내 클래스의 private을 건드릴수있는 함수
+    // friend 클래스는 그 범위를 클래스로 확장해줌
+    // Top-level Function이므로 맨밑에 따로 정의함
+    friend std::ostream& operator<<(std::ostream& out, const MyClass& op);
+    friend std::istream& operator>>(std::istream& in, MyClass& op);
+
+    void setMyNum(int num){
+        this->myNum = num;
+    }
+    int getMyNum() const{
+        return myNum;
+    }
+};
+
+std::ostream& operator<<(std::ostream& out, const MyClass& op){
+    out << "operator<< 호출 : " << op.myNum; // cout이 아니라 매개변수 out인것에 주의
+    return out;
+}
+std::istream& operator>>(std::istream& in, MyClass& op){
+    std::cout << "operator>> 호출" << std::endl;
+    in >> op.myNum;
+    return in;
+}
