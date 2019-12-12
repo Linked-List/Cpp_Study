cb9fa423e4d54226c2fc2b9dc1e0bbe0fd440088
Linked-List <jleey.97@gmail.com>
2019-12-12 17:11:14 +0900

    Week12

--- 2019_2sem_Cpp/Week12/Practice12.cpp
+++ 2019_2sem_Cpp/Week12/Practice12.cpp
@@ -4,45 +4,37 @@
 
 using namespace std;
 
-class myManip{
+class stepws2{
 private:
-    static int width;
     static string fill;
-    string temp = " ";
 
 public:
-    myManip operator()(string fill){
-        myManip t;
-        t.temp = fill;
-        if(fill == ""){
-            myManip::fill = "";
-            myManip::width = 0;
-        }else
-            myManip::fill += fill;
-        return t;
+    stepws2(string fill){
+        if(fill == "")
+            stepws2::fill = "";
+        else
+            stepws2::fill += fill;
     }
 
-    friend ostream& operator<<(ostream&, myManip);
+    friend ostream& operator<<(ostream&, stepws2);
 };
 
-int myManip::width = 1;
-string myManip::fill = "";
+string stepws2::fill = "";
 
-ostream& operator<<(ostream& out, myManip man){
-    if(man.temp == " "){
-        for(int i=0;i<myManip::width;i++)
-            out << ' ';
-        myManip::fill += " ";
-    }else{
-        out << myManip::fill;
-    }
-    myManip::width++;
+ostream& operator<<(ostream& out, stepws2 man){
+    out << stepws2::fill;
+    return out;
+}
+
+ostream& stepws(ostream& out){
+    static int myWidth = 1;
+    for(int i=0;i<myWidth;i++)
+        out << ' ';
+    myWidth++;
     return out;
 }
 
 int main(){
-    myManip stepws;
-
     cout << 10 << endl;
     cout << stepws << 20 << endl;
     cout << stepws << 30 << endl;
@@ -51,15 +43,13 @@ int main(){
     cout << stepws << 60 << endl;
     cout << stepws << 70 << endl;
 
-    cout << stepws(""); // ÃÊ±âÈ­
-
     cout << 10 << endl;
-    cout << stepws("#") << 20 << endl;
-    cout << stepws("#") << 30 << endl;
-    cout << stepws("#") << 40 << endl;
-    cout << stepws("") << 50 << endl;
-    cout << stepws("*") << 60 << endl;
-    cout << stepws("$") << 70 << endl;
+    cout << stepws2("#") << 20 << endl;
+    cout << stepws2("#") << 30 << endl;
+    cout << stepws2("#") << 40 << endl;
+    cout << stepws2("") << 50 << endl;
+    cout << stepws2("*") << 60 << endl;
+    cout << stepws2("$") << 70 << endl;
 
     return 0;
 }
