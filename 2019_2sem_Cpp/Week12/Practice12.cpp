0f12f04ac9a609e2d9bd018b06d671910c48c28e
Linked-List <jleey.97@gmail.com>
2019-12-12 17:37:21 +0900

    Week12

--- 2019_2sem_Cpp/Week12/Practice12.cpp
+++ 2019_2sem_Cpp/Week12/Practice12.cpp
@@ -4,33 +4,37 @@
 
 using namespace std;
 
-class stepws2{
+class myManip{
 private:
     static string fill;
 
 public:
-    stepws2(string fill){
+    myManip(string fill){
         if(fill == "")
-            stepws2::fill = "";
+            myManip::fill = "";
         else
-            stepws2::fill += fill;
+            myManip::fill += fill;
     }
 
-    friend ostream& operator<<(ostream&, stepws2);
+    friend ostream& operator<<(ostream&, myManip);
 };
 
-string stepws2::fill = "";
-
-ostream& operator<<(ostream& out, stepws2 man){
-    out << stepws2::fill;
-    return out;
-}
+string myManip::fill = "";
 
 ostream& stepws(ostream& out){
     static int myWidth = 1;
     for(int i=0;i<myWidth;i++)
         out << ' ';
     myWidth++;
+    return out;
+}
+
+myManip stepws(string fill){
+    return myManip(fill);
+}
+
+ostream& operator<<(ostream& out, myManip man){
+    out << myManip::fill;
     return out;
 }
 
@@ -44,12 +48,12 @@ int main(){
     cout << stepws << 70 << endl;
 
     cout << 10 << endl;
-    cout << stepws2("#") << 20 << endl;
-    cout << stepws2("#") << 30 << endl;
-    cout << stepws2("#") << 40 << endl;
-    cout << stepws2("") << 50 << endl;
-    cout << stepws2("*") << 60 << endl;
-    cout << stepws2("$") << 70 << endl;
+    cout << stepws("#") << 20 << endl;
+    cout << stepws("#") << 30 << endl;
+    cout << stepws("#") << 40 << endl;
+    cout << stepws("") << 50 << endl;
+    cout << stepws("*") << 60 << endl;
+    cout << stepws("$") << 70 << endl;
 
     return 0;
 }
